b7a04c2be7c9aabc454fa211e54fee6793485d5a
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-09-08 09:33:26 +0700

    feat: add Android quick settings voice tile

---
+++ apps/android-satellite/app/src/main/java/com/assisstant/voicesatellite/VoiceTileService.kt
@@ -0,0 +1,26 @@
+package com.assisstant.voicesatellite
+
+import android.content.Intent
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class VoiceTileService : TileService() {
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile?.apply {
+            state = Tile.STATE_ACTIVE
+            label = "Assistant Voice"
+            subtitle = "Nói lệnh"
+            updateTile()
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onClick() {
+        super.onClick()
+        val intent = Intent(this, MainActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            .putExtra(MainActivity.EXTRA_START_VOICE, true)
+        startActivityAndCollapse(intent)
+    }
+}
